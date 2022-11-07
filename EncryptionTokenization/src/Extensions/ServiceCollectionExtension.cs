8d772d79d379767fe576a38297513c1eff9ae69f
Bikram Swain <bikramswain1992@gmail.com>
2022-11-07 13:47:16 +0000

    Added common service collection extension which can be used to add singleton instance of all services within the package.

---
+++ EncryptionTokenization/src/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Encryption.Tokenization;
+using Encryption.Tokenization.Contract;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EncryptionTokenization.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddEncryptionTokenization(IServiceCollection services)
+    {
+        services.AddSingleton<IEncoderDecoder, EncoderDecoder>();
+        services.AddSingleton<IAsymmetricKeyEncoderDecoder, AsymmetricKeyEncoderDecoder>();
+        services.AddSingleton<IJWTTokenizer, JWTTokenizer>();
+
+        return services;
+    }
+}
