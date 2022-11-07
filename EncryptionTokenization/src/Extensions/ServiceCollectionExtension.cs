8cc62c1995c10c0622094c3cf2f0b4332aa5e248
Bikram Swain <bikramswain1992@gmail.com>
2022-11-07 14:05:28 +0000

    Added Scoped and transient service collection extension.

--- EncryptionTokenization/src/Extensions/ServiceCollectionExtension.cs
+++ EncryptionTokenization/src/Extensions/ServiceCollectionExtension.cs
@@ -6,11 +6,29 @@ namespace EncryptionTokenization.Extensions;
 
 public static class ServiceCollectionExtension
 {
-    public static IServiceCollection AddEncryptionTokenization(IServiceCollection services)
+    public static IServiceCollection AddEncryptionTokenizationSingleton(this IServiceCollection services)
     {
         services.AddSingleton<IEncoderDecoder, EncoderDecoder>();
         services.AddSingleton<IAsymmetricKeyEncoderDecoder, AsymmetricKeyEncoderDecoder>();
         services.AddSingleton<IJWTTokenizer, JWTTokenizer>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddEncryptionTokenizationScoped(this IServiceCollection services)
+    {
+        services.AddScoped<IEncoderDecoder, EncoderDecoder>();
+        services.AddScoped<IAsymmetricKeyEncoderDecoder, AsymmetricKeyEncoderDecoder>();
+        services.AddScoped<IJWTTokenizer, JWTTokenizer>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddEncryptionTokenizationTransient(this IServiceCollection services)
+    {
+        services.AddTransient<IEncoderDecoder, EncoderDecoder>();
+        services.AddTransient<IAsymmetricKeyEncoderDecoder, AsymmetricKeyEncoderDecoder>();
+        services.AddTransient<IJWTTokenizer, JWTTokenizer>();
 
         return services;
     }
